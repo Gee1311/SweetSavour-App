ab546bb9099ecf0ac04eee477891db9cdf141356
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-05-26 14:29:09 +0300

    NavHost for navigation between screens

--- app/src/main/java/com/delusional_bear/dessertmaniac/ui/DessertManiacApp.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/ui/DessertManiacApp.kt
@@ -1,41 +1,52 @@
 package com.delusional_bear.dessertmaniac.ui
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.dp
-import com.delusional_bear.dessertmaniac.R
-import com.delusional_bear.dessertmaniac.model.DataSource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.delusional_bear.dessertmaniac.data.DessertManiac
+import com.delusional_bear.dessertmaniac.ui.screens.StartScreen
+
+const val mainActivityLogcatTitle = "MainActivity"
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DessertManiacApp(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    val backStackEntry = navController.currentBackStackEntryAsState()
     Scaffold(
         topBar = {
             DessertManiacTopAppBar()
         },
         content = { paddingValues ->
-            LazyVerticalStaggeredGrid(
-                contentPadding = paddingValues,
-                columns = StaggeredGridCells.Fixed(2),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            Column(
                 modifier = modifier
                     .fillMaxSize()
-                    .padding(dimensionResource(id = R.dimen.padding_small)),
-                verticalItemSpacing = dimensionResource(id = R.dimen.padding_medium),
+                    .padding(paddingValues)
             ) {
-                items(DataSource.dessertList) { dessert ->
-                    DessertCard(dessert = dessert)
+                NavHost(
+                    navController = navController,
+                    startDestination = DessertManiac.Start.name,
+                    modifier = modifier
+                ) {
+                    composable(route = DessertManiac.Start.name) {
+                        StartScreen(
+                            onTopMostPopularClick = { navController.navigate(DessertManiac.TopMostPopular.name) },
+                            onTopMostRatedClick = { navController.navigate(DessertManiac.TopMostRated.name) },
+                            onTopWorstRatedClick = { navController.navigate(DessertManiac.TopWorstRated.name) },
+                            onAllClick = { navController.navigate(DessertManiac.All.name) }
+                        )
+                    }
                 }
             }
         },
     )
 }
+
