1178d03b3399bc8333cae261e78dbb774c27610b
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-05-28 18:49:03 +0300

    Preview composables added

--- app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/AllScreen.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/AllScreen.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.delusional_bear.dessertmaniac.R
 import com.delusional_bear.dessertmaniac.data.Dessert
+import com.delusional_bear.dessertmaniac.model.DataSource
 import com.delusional_bear.dessertmaniac.ui.elements.DessertCard
+import com.delusional_bear.dessertmaniac.ui.theme.DessertManiacTheme
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -27,5 +30,20 @@ fun AllScreen(
         verticalItemSpacing = 16.dp,
     ) {
         items(listOfDesserts) { dessert -> DessertCard(dessert = dessert) }
+    }
+}
+
+@Preview
+@Composable
+private fun AllScreenLightThemePreview() {
+    DessertManiacTheme {
+        AllScreen(listOfDesserts = DataSource.dessertList)
+    }
+}
+@Preview
+@Composable
+private fun AllScreenDarkThemePreview() {
+    DessertManiacTheme(darkTheme = true) {
+        AllScreen(listOfDesserts = DataSource.dessertList)
     }
 }
