0cd9b8df23428c8c58bddf76facf0b3ab4c721cb
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-05-28 18:49:40 +0300

    Updated StartScreen

--- app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/StartScreen.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/StartScreen.kt
@@ -1,30 +1,33 @@
 package com.delusional_bear.dessertmaniac.ui.screens
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.delusional_bear.dessertmaniac.R
-import com.delusional_bear.dessertmaniac.model.DataSource.mainScreenRandomImagesList
+import com.delusional_bear.dessertmaniac.model.DataSource
 import com.delusional_bear.dessertmaniac.model.DataSource.mainScreenRandomSloganList
-import com.delusional_bear.dessertmaniac.ui.common_elements.DessertManiacCategoryButton
+import com.delusional_bear.dessertmaniac.ui.common_elements.DessertManiacCategoryCard
+import com.delusional_bear.dessertmaniac.ui.theme.DessertManiacTheme
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun StartScreen(
     modifier: Modifier = Modifier,
@@ -33,48 +36,105 @@ fun StartScreen(
     onTopWorstRatedClick: () -> Unit,
     onAllClick: () -> Unit,
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
         Card(
             elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-            modifier = Modifier.padding(30.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = 30.dp,
+                    vertical = dimensionResource(id = R.dimen.padding_small)
+                ),
         ) {
-            Column(modifier = Modifier) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(id = R.dimen.padding_medium))
+            ) {
                 Text(
                     text = stringResource(id = mainScreenRandomSloganList.random()),
                     style = MaterialTheme.typography.displaySmall,
                     modifier = Modifier.padding(10.dp),
                     textAlign = TextAlign.Center,
-                    lineHeight = 24.sp,
-                )
-                Image(
-                    painter = painterResource(id = mainScreenRandomImagesList.random()),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth(),
-                    contentScale = ContentScale.Crop
+                    lineHeight = 26.sp,
                 )
             }
         }
-        Column(
-            modifier = modifier,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Fixed(2),
+            verticalItemSpacing = 8.dp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = dimensionResource(id = R.dimen.padding_large),
+                    start = dimensionResource(id = R.dimen.padding_small),
+                    bottom = dimensionResource(id = R.dimen.padding_small),
+                    end = dimensionResource(id = R.dimen.padding_small),
+                ),
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
         ) {
-            DessertManiacCategoryButton(
-                buttonTitle = R.string.top_most_popular,
-                onClick = onTopMostPopularClick
-            )
-            DessertManiacCategoryButton(
-                buttonTitle = R.string.top_most_rated,
-                onClick = onTopMostRatedClick
-            )
-            DessertManiacCategoryButton(
-                buttonTitle = R.string.top_worst_rated,
-                onClick = onTopWorstRatedClick
-            )
-            DessertManiacCategoryButton(
-                buttonTitle = R.string.all_desserts,
-                onClick = onAllClick
-            )
+            items(DataSource.categoryAndImageList) { index ->
+                when (index.first) {
+                    R.string.top_most_popular -> {
+                        DessertManiacCategoryCard(
+                            title = index.first,
+                            image = index.second,
+                            onClick = onTopMostPopularClick,
+                        )
+                    }
+                    R.string.top_most_rated -> {
+                        DessertManiacCategoryCard(
+                            title = index.first,
+                            image = index.second,
+                            onClick = onTopMostRatedClick,
+                        )
+                    }
+                    R.string.top_worst_rated -> {
+                        DessertManiacCategoryCard(
+                            title = index.first,
+                            image = index.second,
+                            onClick = onTopWorstRatedClick,
+                        )
+                    }
+                    R.string.all_desserts -> {
+                        DessertManiacCategoryCard(
+                            title = index.first,
+                            image = index.second,
+                            onClick = onAllClick,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun StartScreenLightThemePreview() {
+    DessertManiacTheme {
+        StartScreen(
+            onTopMostPopularClick = { /*TODO*/ },
+            onTopMostRatedClick = { /*TODO*/ },
+            onTopWorstRatedClick = { /*TODO*/ },
+        ) {
+
+        }
+    }
+}
+@Preview
+@Composable
+private fun StartScreenDarkThemePreview() {
+    DessertManiacTheme(darkTheme = true) {
+        StartScreen(
+            onTopMostPopularClick = { /*TODO*/ },
+            onTopMostRatedClick = { /*TODO*/ },
+            onTopWorstRatedClick = { /*TODO*/ },
+        ) {
+
         }
     }
 }
