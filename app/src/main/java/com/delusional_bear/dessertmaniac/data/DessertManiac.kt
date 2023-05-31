0ea6267c18ceede0be745263409c79bbe8687c96
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-05-31 12:57:31 +0300

    Updated enum class for displaying titles in top bar

--- app/src/main/java/com/delusional_bear/dessertmaniac/data/DessertManiac.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/data/DessertManiac.kt
@@ -10,6 +10,10 @@ enum class DessertManiac(@StringRes val title: Int) {
     TopMostRated(title = R.string.top_most_rated),
     TopMostPopular(title = R.string.top_most_popular),
     TopWorstRated(title = R.string.top_worst_rated),
-    All(title = R.string.all_desserts),
+    All(title = R.string.all_desserts_top_bar),
     Start(title = R.string.app_name),
+    Location(title = R.string.location),
+    Country(title = R.string.country),
+    UnitedStates(title = R.string.united_states),
+    Ukraine(title = R.string.ukraine),
 }
