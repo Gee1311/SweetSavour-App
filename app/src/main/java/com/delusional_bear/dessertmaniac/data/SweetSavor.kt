fc68e32d84421d80b10ebbffdded91351af1f822
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-06-15 10:52:07 +0300

    Class that represents different screens of the app

---
+++ app/src/main/java/com/delusional_bear/dessertmaniac/data/SweetSavor.kt
@@ -0,0 +1,14 @@
+package com.delusional_bear.dessertmaniac.data
+
+import androidx.annotation.StringRes
+import com.delusional_bear.dessertmaniac.R
+
+/**
+ * [SweetSavor] enum class represents the titles of different app's screens
+ */
+enum class SweetSavor(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Continent(title = R.string.continent),
+    Country(title = R.string.country),
+    Profile(title = R.string.profile),
+}
