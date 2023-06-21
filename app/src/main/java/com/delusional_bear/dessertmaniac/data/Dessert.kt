8e97e21b63aea1a93de73c551f1d2f6cdeca0aab
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-06-21 20:00:18 +0300

    Updated Dessert class

--- app/src/main/java/com/delusional_bear/dessertmaniac/data/Dessert.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/data/Dessert.kt
@@ -7,12 +7,11 @@ import androidx.annotation.StringRes
  * [Dessert] data class to represent a dessert
  */
 
-data class Dessert(
+class Dessert(
     @StringRes val dessertName: Int,
     @DrawableRes val dessertImage: Int,
     @StringRes val ingredients: List<Int>,
     val rating: Double,
-    val preparationTime: Int = 0,
-    val cookTime: Int = 0,
-    val nutrition: List<Pair<String, Int>> = emptyList()
+    val price: String,
+    val totalNumberOfOrders: Int = 0,
 )
