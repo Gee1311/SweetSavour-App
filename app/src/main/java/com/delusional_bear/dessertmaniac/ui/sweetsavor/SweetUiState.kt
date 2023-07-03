aa361a01ef3ce1eeb19bda68a0d44c76ff4b3850
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-03 15:59:12 +0300

    UI state was added to display the data on screen

---
+++ app/src/main/java/com/delusional_bear/dessertmaniac/ui/sweetsavor/SweetUiState.kt
@@ -0,0 +1,20 @@
+package com.delusional_bear.dessertmaniac.ui.sweetsavor
+
+import com.delusional_bear.dessertmaniac.data.Country
+import com.delusional_bear.dessertmaniac.data.Dessert
+import com.delusional_bear.dessertmaniac.data.model.DataSource
+
+data class SweetUiState(
+    val isDessertAvailable: Boolean = false,
+    val continent: Int = 0,
+    val countries: Map<Int, List<Country>> = DataSource.countryList.groupBy { it.continentName },
+    val favoriteDesserts: MutableSet<Dessert> = mutableSetOf(),
+    val cartDesserts: MutableSet<Dessert> = mutableSetOf(),
+    val dessertCount: Int = 1,
+    val subTotal: Double = 0.0,
+    val shipping: Double = 2.99,
+    val discount: Double = 0.0,
+    val balance: Double = 140.0,
+    val totalItems: Int = 0,
+    val isLogoutDialogOpened: Boolean = false,
+)
