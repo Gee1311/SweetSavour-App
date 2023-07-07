26f8caa3dd57f7755727992a6be2bb0dc3e9f888
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-07 14:25:45 +0300

    A slightly changed alert dialog. Experimental alert dialog was replaced with stable one

--- app/src/main/java/com/delusional_bear/dessertmaniac/ui/elements/dialogs/DessertAlertDialog.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/ui/elements/dialogs/DessertAlertDialog.kt
@@ -1,21 +1,12 @@
 package com.delusional_bear.dessertmaniac.ui.elements.dialogs
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import android.content.Context
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.AlertDialogDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,56 +14,49 @@ import androidx.compose.ui.unit.sp
 import com.delusional_bear.dessertmaniac.R
 import com.delusional_bear.dessertmaniac.data.Dessert
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DessertAlertDialog(
     modifier: Modifier = Modifier,
+    context: Context,
     dessert: Dessert,
-    onDialogDismiss: () -> Unit,
-    onConfirmClick: () -> Unit,
-    onCancelClick: () -> Unit,
+    onDialogDismiss: () -> Unit = {},
+    onConfirmClick: () -> Unit = {},
+    onDismissClick: () -> Unit = {},
 ) {
     AlertDialog(
         onDismissRequest = onDialogDismiss,
-        content = {
-            Surface(
-                modifier = modifier
-                    .wrapContentWidth()
-                    .wrapContentHeight(),
-                shape = MaterialTheme.shapes.large,
-                tonalElevation = AlertDialogDefaults.TonalElevation
+        confirmButton = {
+            Button(
+                onClick = { onConfirmClick.invoke() },
+                shape = RoundedCornerShape(8.dp)
             ) {
-                Column(modifier = modifier.padding(16.dp)) {
-                    Text(
-                        text = stringResource(id = R.string.dessert_added, stringResource(id = dessert.dessertName)),
-                        style = MaterialTheme.typography.bodyMedium,
-                        lineHeight = 22.sp,
-                    )
-                    Spacer(modifier = modifier.height(24.dp))
-                    Row(
-                        modifier = modifier.align(Alignment.End),
-                    ) {
-                        TextButton(
-                            onClick = onCancelClick,
-                            modifier = modifier,
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.cancel),
-                                style = MaterialTheme.typography.labelMedium,
-                            )
-                        }
-                        TextButton(
-                            onClick = onConfirmClick,
-                            modifier = modifier,
-                        ) {
-                            Text(
-                                text = stringResource(id = R.string.confirm),
-                                style = MaterialTheme.typography.labelMedium,
-                            )
-                        }
-                    }
-                }
+                Text(
+                    text = stringResource(id = R.string.confirm),
+                    style = MaterialTheme.typography.labelMedium,
+                )
             }
         },
+        dismissButton = {
+            Button(
+                onClick = { onDismissClick.invoke() },
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.cancel),
+                    style = MaterialTheme.typography.labelMedium,
+                )
+            }
+        },
+        modifier = modifier,
+        text = {
+            Text(
+                text = stringResource(
+                    id = R.string.dessert_added,
+                    context.resources.getString(dessert.dessertName)
+                ),
+                style = MaterialTheme.typography.titleSmall,
+                lineHeight = 24.sp,
+            )
+        }
     )
 }
