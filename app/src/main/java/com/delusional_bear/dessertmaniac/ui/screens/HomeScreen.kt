5e616988e56d584bca05427739aeed0056be665d
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-08 13:04:52 +0300

    Added a delay when user taps on "Add to cart" button to imitate the processing of user request and after adding a dessert to the cart toast message displays

--- app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/HomeScreen.kt
@@ -1,24 +1,32 @@
 package com.delusional_bear.dessertmaniac.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.delusional_bear.dessertmaniac.R
 import com.delusional_bear.dessertmaniac.data.model.DataSource
+import com.delusional_bear.dessertmaniac.ui.common_functions.displayToastMessage
 import com.delusional_bear.dessertmaniac.ui.elements.pagers.DessertsLazyRow
 import com.delusional_bear.dessertmaniac.ui.sweetsavor.SweetSavorViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
     sweetSavorViewModel: SweetSavorViewModel,
 ) {
+    val coroutineScope = rememberCoroutineScope()
+    val context = LocalContext.current
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -33,7 +41,13 @@ fun HomeScreen(
             dessertList = DataSource.dessertList
                 .filter { it.totalNumberOfOrders >= 650 }
                 .sortedByDescending { it.totalNumberOfOrders },
-            onAddToCartButtonClick = { sweetSavorViewModel.addToCart(it) },
+            onAddToCartButtonClick = {
+                coroutineScope.launch {
+                    delay(500L)
+                    sweetSavorViewModel.addToCart(it)
+                    displayToastMessage(context, R.string.dessert_added_to_cart, Toast.LENGTH_SHORT)
+                }
+            },
         ) {
             sweetSavorViewModel.addToFavorites(it)
         }
