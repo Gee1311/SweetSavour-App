c4834aeb3aac547de025d0de67a92272a85d5901
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-07-08 16:04:45 +0300

    BottomSheetScaffold was added. It allows to add BottomSheet where payment methods are display

--- app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/CartScreen.kt
+++ app/src/main/java/com/delusional_bear/dessertmaniac/ui/screens/CartScreen.kt
@@ -1,38 +1,47 @@
 package com.delusional_bear.dessertmaniac.ui.screens
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.delusional_bear.dessertmaniac.R
 import com.delusional_bear.dessertmaniac.data.Dessert
+import com.delusional_bear.dessertmaniac.ui.common_elements.DescriptionText
 import com.delusional_bear.dessertmaniac.ui.common_functions.convertDoubleToCurrency
 import com.delusional_bear.dessertmaniac.ui.common_functions.totalPriceOfOrderedDesserts
 import com.delusional_bear.dessertmaniac.ui.elements.cards.CartCard
 import com.delusional_bear.dessertmaniac.ui.sweetsavor.SweetSavorViewModel
 import com.delusional_bear.dessertmaniac.ui.sweetsavor.SweetUiState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CartScreen(
     uiState: SweetUiState,
@@ -40,45 +49,90 @@ fun CartScreen(
     modifier: Modifier = Modifier,
     onBackPressed: () -> Unit = {},
 ) {
-    BackHandler { onBackPressed.invoke()  }
-    Column(modifier = modifier
-        .fillMaxSize()
-        .padding(dimensionResource(id = R.dimen.padding_large))
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = SheetState(
+            initialValue = SheetValue.PartiallyExpanded,
+            skipPartiallyExpanded = false,
+        )
+    )
+    BackHandler { onBackPressed.invoke() }
+    BottomSheetScaffold(
+        sheetContent = { CartBottomSheet() },
+        scaffoldState = bottomSheetScaffoldState,
+        sheetPeekHeight = 30.dp,
     ) {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium)),
-            modifier = Modifier.height(370.dp)
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(dimensionResource(id = R.dimen.padding_large))
         ) {
-            items(uiState.cartDesserts.toList()) { dessert ->
-                CartCard(
-                    dessert = dessert,
-                    count = uiState.dessertCount,
-                    onMinusButtonClick = { viewModel.minusDessert() },
-                    onAddButtonClick = { viewModel.plusDessert() },
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium)),
+                modifier = Modifier.height(370.dp)
+            ) {
+                items(uiState.cartDesserts.toList()) { dessert ->
+                    CartCard(
+                        dessert = dessert,
+                        count = uiState.dessertCount,
+                        onMinusButtonClick = { viewModel.minusDessert() },
+                        onAddButtonClick = { viewModel.plusDessert() },
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            OrderPrice(
+                text = R.string.sub_total_text,
+                price = convertDoubleToCurrency(uiState.subTotal)
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            OrderPrice(
+                text = R.string.shipping_text,
+                price = convertDoubleToCurrency(
+                    if (uiState.cartDesserts.size > 0) uiState.shipping else 0.0
+                )
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            OrderPrice(
+                text = R.string.discount,
+                price = convertDoubleToCurrency(uiState.discount)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Divider(thickness = 2.dp)
+            Spacer(modifier = Modifier.height(16.dp))
+            TotalPrice(uiState = uiState)
+        }
+    }
+}
+
+// TODO: To change CartBottomSheet so that user can choose whether an order must be delivered to home, when they want to pick up the dessert, payment methods etc.
+
+@Composable
+private fun CartBottomSheet(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(1f)
+            .fillMaxHeight(0.55f)
+            .padding(dimensionResource(id = R.dimen.padding_large)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        DescriptionText(text = R.string.payment_method)
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small)),
+        ) {
+            val paymentOptions = listOf(
+                R.drawable.paypal,
+                R.drawable.google,
+                R.drawable.apple,
+            )
+            items(paymentOptions) { payment ->
+                Image(
+                    painter = painterResource(id = payment),
+                    contentDescription = null,
+                    modifier = Modifier.size(80.dp)
                 )
             }
         }
-        Spacer(modifier = Modifier.height(12.dp))
-        OrderPrice(
-            text = R.string.sub_total_text,
-            price = convertDoubleToCurrency(uiState.subTotal)
-        )
-        Spacer(modifier = Modifier.height(12.dp))
-        OrderPrice(
-            text = R.string.shipping_text,
-            price = convertDoubleToCurrency(
-                if (uiState.cartDesserts.size > 0) uiState.shipping else 0.0
-            )
-        )
-        Spacer(modifier = Modifier.height(12.dp))
-        OrderPrice(
-            text = R.string.discount,
-            price = convertDoubleToCurrency(uiState.discount)
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Divider(thickness = 2.dp)
-        Spacer(modifier = Modifier.height(16.dp))
-        TotalPrice(uiState = uiState)
     }
 }
 
@@ -107,7 +161,6 @@ private fun OrderPrice(
 private fun TotalPrice(
     uiState: SweetUiState,
     modifier: Modifier = Modifier,
-    onProceedToCheckoutButtonClick: () -> Unit = {},
 ) {
     Card(
         modifier = modifier.fillMaxSize(),
@@ -121,7 +174,11 @@ private fun TotalPrice(
                     .padding(dimensionResource(id = R.dimen.padding_medium)),
                 horizontalArrangement = Arrangement.SpaceBetween,
             ) {
-                Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
                     Text(
                         text = stringResource(
                             id = R.string.total_text,
@@ -132,23 +189,15 @@ private fun TotalPrice(
                     Text(
                         text = stringResource(
                             id = R.string.total_price,
-                            convertDoubleToCurrency(displayTotalPriceForOrder(
-                                uiState.cartDesserts.toList(),
-                                uiState.shipping,
-                                uiState.discount
-                            ))
+                            convertDoubleToCurrency(
+                                displayTotalPriceForOrder(
+                                    uiState.cartDesserts.toList(),
+                                    uiState.shipping,
+                                    uiState.discount
+                                )
+                            )
                         ),
                         style = MaterialTheme.typography.displayMedium,
-                    )
-                }
-                Button(
-                    onClick = { onProceedToCheckoutButtonClick.invoke() },
-                    shape = RoundedCornerShape(10.dp),
-                    contentPadding = PaddingValues(14.dp)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.proceed_to_checkout),
-                        style = MaterialTheme.typography.titleSmall,
                     )
                 }
             }
